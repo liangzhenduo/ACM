e9e64f2c13c854f96ebf1b542a3d43298829a138
Shintaku <liangzhenduo0608@hotmail.com>
2015-07-15 14:41:18 +0800

    HDU
    栈&队列

--- HDUOJ/1022.Train Problem I.cpp
+++ HDUOJ/1022.Train Problem I.cpp
@@ -1,33 +1,31 @@
+#include <cstring>
 #include <cstdio>
+#include <string>
+#include <vector>
 #include <stack>
-#include <algorithm>
+#include <queue>
 using namespace std;
 int main(){
     char a[1001],b[1001];
     int n,i,j,cnt,c[1001];
     while(scanf("%d",&n)!=EOF){
-        stack<char>s;
+        stack<char> s;
+        queue<char> q;
+        vector<int> v;
         getchar();
         scanf("%s%s",a,b);
-        for(cnt=i=j=0;i<n;i++){
+        for(i=0;i<n;i++) q.push(b[i]);
+        for(i=0;i<n;i++){
             s.push(a[i]);
-            c[cnt++]=1;
-            while(j<n&&!s.empty())
-                if(s.top()==b[j]){
-                    s.pop();
-                    c[cnt++]=0;
-                    j++;
-                }
-                else break;
+            v.push_back(1);
+            while(!s.empty()&&s.top()==q.front())
+                s.pop(),q.pop(),v.push_back(0);
         }
-        if(s.empty()){
+        if(!s.empty()) puts("No.");
+        else{
             puts("Yes.");
-            for(i=0;i<cnt;i++){
-                if(c[i]) puts("in");
-                else puts("out");
-            }
+            for(i=0;i<2*n;i++) puts(v[i]?"in":"out");
         }
-        else puts("No.");
         puts("FINISH");
     }
 }
